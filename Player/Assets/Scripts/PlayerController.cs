3549f9ab31791a0efece81c334cf58fa49a8ee73
Samuel Sarette <samuelsarette@linux.com>
2020-01-25 17:09:48 -0500

    update movement scripts, make scripts folder

---
+++ Player/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _playerBody;    
+    private float WalkSpeed = 40f;
+    private float TurnSpeed = 180f;
+
+    private float rotate;
+    private float z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerBody = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+ 
+        if (Input.GetMouseButton (0)) {
+
+            var halfWidth = (Screen.width / 2);
+            var halfHeight = (Screen.height / 2);
+
+            var directionX = Input.mousePosition.x < halfWidth ? -1 : 1;
+            var directionY = Input.mousePosition.y < halfHeight ? -1 : 1;
+
+            rotate = (Input.mousePosition.x - halfWidth) / halfWidth;
+            z = (Input.mousePosition.y - halfHeight) / halfHeight;
+        } else {
+            rotate = Input.GetAxis("Horizontal");
+            z = Input.GetAxis("Vertical");
+        }
+        
+        Debug.Log($"New {rotate}, {z}");
+        rotate = rotate * TurnSpeed * Time.deltaTime;
+        z = z * WalkSpeed * Time.deltaTime; 
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        transform.Rotate(0, rotate, 0);
+        transform.Translate(new Vector3 (0, 0, z));
+    }
+
+}
