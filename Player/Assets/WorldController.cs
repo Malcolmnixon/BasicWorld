43918dbb3bfcb0837c544e27142d139938af25ef
Samuel Sarette <samuelsarette@linux.com>
2020-01-25 14:42:46 -0500

    Added basic scene with prefab instantiation

---
+++ Player/Assets/WorldController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour
+{
+
+    [Header("Prefabs")]
+    
+    [SerializeField]
+    public GameObject PlayerPrefab;
+    
+    [Space]
+
+    [SerializeField]
+    public GameObject RemotePlayerPrefab;
+
+    private GameObject Player;
+
+    private GameObject[] RemotePlayers;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = Instantiate(PlayerPrefab);
+        Player.transform.SetParent(transform);
+        Camera.main.transform.SetParent(Player.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
